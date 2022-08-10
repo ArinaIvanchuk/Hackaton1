70b82133289601b784b00513fd0774010399b8ca
Arina <arina_ivanchyk@mail.ru>
2022-08-10 17:25:43 +0300

    add all tasks in one file

--- Program.cs
+++ Program.cs
@@ -1,6 +1,50 @@
 ﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-Console.WriteLine("input number");
-int num1 = Convert.ToInt32(Console.ReadLine());
-int first = num1/10;
-int second = first%10;
-Console.WriteLine(second);
+// Console.WriteLine("input number");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// int first = num1/10;
+// int second = first%10;
+// Console.WriteLine(second);
+
+
+//  //Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//  Console.WriteLine("input number");
+//  int num1 = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+//  while (1000 <= num1)
+//  {
+//     num1 = num1/10;
+   
+//  }
+// if (num1 < 1000 && num1 > 99)
+// {
+//       num1 = num1 % 10;
+//       Console.WriteLine(num1 + " third digit number"); 
+
+// }   
+// else
+// {
+//    Console.WriteLine(num1 + " does not have third digit");
+// }
+
+
+
+// // Напишите программу, которая принимает на вход цифру, обозначающую день 
+// //недели, и проверяет, является ли этот день выходным.
+
+// Console.Write("input number");
+// int day = Convert.ToInt32(Console.ReadLine());
+// if (day < 1 || day > 7)
+// {
+//     Console.WriteLine("incorrect number");
+// }
+// else if (day == 6)
+// {
+//     Console.WriteLine("yes, it is Saturday");
+// }
+// else if (day == 7)
+// {
+//     Console.WriteLine("yes, it is Sunday");
+// }
+// else
+// {
+//     Console.WriteLine("no, it is not weekend");
+// }
