89874dfd1c04e7f4791b72ec936770180898b624
Arina <arina_ivanchyk@mail.ru>
2022-08-10 16:48:57 +0300

    add file

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("input number");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int first = num1/10;
+int second = first%10;
+Console.WriteLine(second);
